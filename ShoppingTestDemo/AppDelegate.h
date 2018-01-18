f2d6ad7b548678b094f93a415a75048eb62ffae6
赵龙杰 <zhaolongjie@zhaolongjiedeMacBook-Pro.local>
2018-01-18 10:47:03 +0800

    0.0.1

---
+++ ShoppingTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingTestDemo
+//
+//  Created by 赵龙杰 on 2018/1/18.
+//  Copyright © 2018年 longjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingTestDemo
+//
+//  Created by 赵龙杰 on 2018/1/18.
+//  Copyright © 2018年 longjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
