072918b75ea7cce86466eb70c38621f951aa6f5b
TomLongJoy <471727371@qq.com>
2018-01-18 16:50:52 +0800

    初始化tableview
    1.tableview
    2.需要添加model，（本想自己写个，水平有限，打算用第三方MJ的Model）

--- ShoppingTestDemo/ViewController.m
+++ ShoppingTestDemo/ViewController.m
@@ -8,22 +8,81 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property (nonatomic,strong)    UITableView     *shoppingCartTableView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.shoppingCartTableView];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableView *)shoppingCartTableView{
+    if (!_shoppingCartTableView) {
+        CGRect   tabRect = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+        _shoppingCartTableView = [[UITableView alloc] initWithFrame:tabRect style:UITableViewStylePlain];
+        _shoppingCartTableView.delegate = self;
+        _shoppingCartTableView.dataSource = self;
+        _shoppingCartTableView.estimatedRowHeight = 0;
+        _shoppingCartTableView.tableFooterView = [UIView new];
+    }
+    return _shoppingCartTableView;
 }
-
-
+#pragma mark -- UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 10;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return [UITableViewCell new];
+}
+#pragma mark -- UITableviewDelegate
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 50;
+}
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
